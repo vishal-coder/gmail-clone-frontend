eeede552cb3c69afc743b774396a4fe7a47e22cb
vishal Coder <[vishal2app@gmail.com]>
2022-08-24 19:10:16 +0530

    getting mail by email type

--- src/components/EmailType.js
+++ src/components/EmailType.js
@@ -1,20 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import InboxOutlinedIcon from "@mui/icons-material/InboxOutlined";
 import PeopleAltOutlinedIcon from "@mui/icons-material/PeopleAltOutlined";
 import LocalOfferOutlinedIcon from "@mui/icons-material/LocalOfferOutlined";
+import { useDispatch } from "react-redux";
+import { openComposeModal } from "../features/composeMailSlice";
+import { getLabelList } from "../services/LabelService.js";
+import { setMailList, setMailListLoading } from "../features/mailListSlice.js";
+import { getMailList } from "../services/MailService.js";
 
 function EmailType() {
+  const [active, setActive] = useState("INBOX");
+  const dispatch = useDispatch();
+  const getMailByLabel = async (labelType) => {
+    dispatch(setMailListLoading(true));
+    setActive(labelType);
+    console.log("inside getMailByLabel", labelType);
+    const token = localStorage.getItem("token");
+    const mails = await getMailList(token, {
+      mailOption: {
+        userId: "me",
+        labelIds: labelType,
+        format: "metadata",
+      },
+    });
+    console.log("getMailByLabel response is ", mails);
+
+    dispatch(setMailList(mails.data));
+    dispatch(setMailListLoading(false));
+  };
   return (
     <div className="emailtype">
-      <div className="emailtypeitem">
+      <div
+        className={`emailtypeitem ${active == "INBOX" && "activeEmailType"}`}
+        onClick={() => {
+          getMailByLabel("INBOX");
+        }}
+      >
         <InboxOutlinedIcon />
         <span>Primary</span>
       </div>
-      <div className="emailtypeitem">
+      <div
+        className={`emailtypeitem ${
+          active == "CATEGORY_SOCIAL" && "activeEmailType"
+        }`}
+        onClick={() => {
+          getMailByLabel("CATEGORY_SOCIAL");
+        }}
+      >
         <PeopleAltOutlinedIcon />
         <span>Social</span>
       </div>
-      <div className="emailtypeitem">
+      <div
+        className={`emailtypeitem ${
+          active == "CATEGORY_PROMOTIONS" && "activeEmailType"
+        }`}
+        onClick={() => {
+          getMailByLabel("CATEGORY_PROMOTIONS");
+        }}
+      >
         <LocalOfferOutlinedIcon />
         <span>Promotion</span>
       </div>
